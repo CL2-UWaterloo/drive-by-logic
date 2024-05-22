3f791a001ec90d2ec3e686f671c36219c62d1727
kvkpraneeth <praneeth.pep@gmail.com>
2024-05-21 23:36:01 -0400

    Improved Formulation

--- workspace/optimal_control/scripts/dubins_vehicle.py
+++ workspace/optimal_control/scripts/dubins_vehicle.py
@@ -7,13 +7,35 @@ from optimal_control.executor import Executor
 from dataclasses import dataclass, asdict
 from casadi import *
 
+# TODO: Refactor Name from state to anything else
 @dataclass
 class State:
+    # Position Information
     x : SX | float
     y : SX | float
     theta : SX | float
-    sigma : SX | float
-    t : SX | float
+
+    # Below information encodes how the robot got to this state
+    # Formulation inspired from: https://ieeexplore.ieee.org/document/9143597
+
+    # Segment 1
+    s0 : SX | float
+    t0 : SX | float
+    
+    # Segment 2
+    s1 : SX | float
+    t1 : SX | float
+
+    # Segment 3
+    s2 : SX | float
+    t2 : SX | float
+
+    # Velocity and Curvature
+    # To imitate Dubin's formulation of a car, the velocity and curvature are constant
+    v : SX | float
+    k : SX | float
+
+# TODO: Should obstacle checking be in terms of forward kinematics of the car model?
 
 class DubinCar(Problem):
 
@@ -23,10 +45,9 @@ class DubinCar(Problem):
         self.prep_robot_information()
 
         # Formulation Parameters
-        self.number_of_states = 5
+        self.number_of_states = 3
 
-        # Stage : Intermediate State
-        self.number_of_stages = 4
+        self.granularity = 5
 
         self.t_max = 40 # s
 
@@ -41,106 +62,202 @@ class DubinCar(Problem):
         self.v = self.max_linear_velocity*0.5
 
     def prep_problem(self, *args, **kwargs):
-        self.states : list[list[State]] = []
+        self.states : list[State] = []
 
         for i in range(self.number_of_states):
-            state_idx = str(i)
-            state = []
-            for j in range(self.number_of_stages):
-                idx = state_idx + str(j)
-                stage = State(
-                    SX.sym("x"+idx), SX.sym("y" + idx), SX.sym("th" + idx),
-                    SX.sym("s" + idx), SX.sym("t" + idx)
-                )
-                for _, value in asdict(stage).items():
-                    self.set_variable(value.name(), value)
-                state.append(stage)
+            idx = str(i)
+            
+            # Make State
+            state = State(
+                SX.sym("x"+idx), SX.sym("y"+idx), SX.sym("th"+idx),
+                SX.sym("s0"+idx), SX.sym("t0"+idx),
+                SX.sym("s1"+idx), SX.sym("t1"+idx),
+                SX.sym("s2"+idx), SX.sym("t2"+idx),
+                SX.sym("v"+idx), SX.sym("k"+idx)
+            )
+            
+            # Declare Decision Variables
+            for _, value in asdict(state).items():
+                self.set_variable(value.name(), value)
+
             self.states.append(state)
 
     def prep_constraints(self, *args, **kwargs):
+        # Get Initial, Final states, and Obstacles from arguments.
         self.initial_state = kwargs["init"]
         self.final_state = kwargs["final"]
         self.obstacles = kwargs["obstacles"]
 
-        X0 = self.states[0][0]; Xn = self.states[-1][-1]
+        # The first state is X0 and the last is Xn
+        X0 = self.states[0]; Xn = self.states[-1]
 
         # Boundary Conditions
         self.set_equality_constraint("x0", X0.x, init.x)
         self.set_equality_constraint("y0", X0.y, init.y)
         self.set_equality_constraint("th0", X0.theta, init.th)
 
-        # TODO : Add tolerances here
+        # Final Boundary Conditions are subject to change based on problem description
+
+        # TODO : Add tolerances here to represent a goal region instead of pose
         self.set_equality_constraint("xn", Xn.x, final.x)
         self.set_equality_constraint("yn", Xn.y, final.y)
+
+        # Orientation is the same for any point reached within goal region
         self.set_equality_constraint("thn", Xn.theta, final.th)
 
-        time_squared_sum = 0
-        for i in range(self.number_of_states):
+        # Defining parametric equations for x, y
+        def parametric_x(l, k, th):
+            arc_phase = k*l*0.5
+            return l*sinc(arc_phase)*cos(th + arc_phase)
+
+        def parametric_y(l, k, th):
+            arc_phase = k*l*0.5
+            return l*sinc(arc_phase)*sin(th + arc_phase)
+
+        # Iterate through all states to establish constraints
+        time_sum = 0
+        for i in range(1, self.number_of_states):
+            idx = str(i)
+
+            # Current state and previous state (Xim1 = Xi-1)
+            Xi = self.states[i]; Xim1 = self.states[i-1]
+
+            # Velocity Selection Constraint
+            self.set_constraint(
+                "v"+idx, Xi.v, 0, self.max_linear_velocity
+            )
+
+            # Acceleration Constraint
+            self.set_constraint(
+                "a"+idx, Xi.v - Xim1.v, 0, self.max_acceleration
+            )
+
+            # Curvature Selection Constraint
+            self.set_constraint(
+                "k"+idx, Xi.k, 0, self.k
+            )
+
+            # Smoothness Constraint
+            # self.set_constraint(
+            #     "k_dot"+idx, Xi.k - Xim1.k, 0, 0.1
+            # )
+
+            # Time Selection Constraint
+            self.set_constraint(
+                "t0"+idx, Xi.t0, 0
+            )
+            self.set_constraint(
+                "t1"+idx, Xi.t1, 0
+            )
+            self.set_constraint(
+                "t2"+idx, Xi.t2, 0
+            )
+
+            # Sigma Selection Constraint
+            self.set_constraint(
+                "s0"+idx, Xi.s0, -1, 1
+            )
+            self.set_constraint(
+                "s1"+idx, Xi.s1, -1, 1
+            )
+            self.set_constraint(
+                "s2"+idx, Xi.s2, -1, 1
+            )
+
+            # Start with establishing the equality constraint between final and initial positions
+            self.set_equality_constraint(
+                "x"+idx,
+                Xi.x - Xim1.x \
+                    - parametric_x(Xi.v*Xi.t0, Xi.k*Xi.s0, Xim1.theta) \
+                    - parametric_x(Xi.v*Xi.t1, Xi.k*Xi.s1, Xim1.theta + Xi.k*Xi.s0*Xi.v*Xi.t0) \
+                    - parametric_x(Xi.v*Xi.t2, Xi.k*Xi.s2, Xim1.theta + Xi.k*Xi.s0*Xi.v*Xi.t0 + Xi.k*Xi.s1*Xi.v*Xi.t1),
+                0
+            )
+
+            self.set_equality_constraint(
+                "y"+idx,
+                Xi.y - Xim1.y \
+                    - parametric_y(Xi.v*Xi.t0, Xi.k*Xi.s0, Xim1.theta) \
+                    - parametric_y(Xi.v*Xi.t1, Xi.k*Xi.s1, Xim1.theta + Xi.k*Xi.s0*Xi.v*Xi.t0) \
+                    - parametric_y(Xi.v*Xi.t2, Xi.k*Xi.s2, Xim1.theta + Xi.k*Xi.s0*Xi.v*Xi.t0 + Xi.k*Xi.s1*Xi.v*Xi.t1),
+                0
+            )
+
+            self.set_equality_constraint(
+                "th"+idx,
+                Xi.theta - Xim1.theta - Xi.k*Xi.s0*Xi.v*Xi.t0 - Xi.k*Xi.s1*Xi.v*Xi.t1 - Xi.k*Xi.s2*Xi.v*Xi.t2,
+                0
+            )
+
+            time_sum += (Xi.t0 + Xi.t1 + Xi.t2)
+
+        self.set_constraint("time_sum", time_sum, 0, self.t_max)
+
+        # Forward calculate trajectory at finer intervals
+        # Reason 1: Obstacle Avoidance.
+        # Reason 2: To get a finer trajectory output out of casadi during solving time.
+        for i in range(1, self.number_of_states):
+            # Similar Setup as before
             state_idx = str(i)
+            Xi = self.states[i]; Xim1 = self.states[i-1]
 
-            for j in range(1, self.number_of_stages):
-                idx = state_idx+str(j)     
-                Xi = self.states[i][j]; Xim1 = self.states[i][j-1]
+            # First Segment Trajectory
+            Xim1_fine = self.trajectory(
+                state = Xim1,
+                v = Xi.v,
+                k = Xi.k,
+                n = self.granularity,
+                s = Xi.s0,
+                t = Xi.t0
+            )
 
-                self.set_constraint("t"+idx, Xi.t, 0)
-                self.set_constraint("s"+idx, Xi.sigma, -1, 1)
+            # Second Segment Trajectory
+            Xim1_fine.extend(self.trajectory(
+                # Start from last state of previously generated trajectory
+                state = Xim1_fine[-1],
+                v = Xi.v,
+                k = Xi.k,
+                n = self.granularity,
+                s = Xi.s1,
+                t = Xi.t1
+            ))
 
-                arc_phase = Xi.sigma*self.k*self.v*Xi.t*0.5
-                self.set_equality_constraint("x"+idx, Xi.x - Xim1.x - self.v*Xi.t*sinc(arc_phase)*cos(Xim1.theta + arc_phase), 0)
-                self.set_equality_constraint("y"+idx, Xi.y - Xim1.y - self.v*Xi.t*sinc(arc_phase)*sin(Xim1.theta + arc_phase), 0)
-                self.set_equality_constraint("th"+idx, Xi.theta - Xim1.theta - 2*arc_phase, 0)
+            # Third Segment Trajectory
+            Xim1_fine.extend(self.trajectory(
+                state = Xim1_fine[-1],
+                v = Xi.v,
+                k = Xi.k,
+                n = self.granularity,
+                s = Xi.s2,
+                t = Xi.t2
+            ))
 
-                time_squared_sum += power(Xi.t, 2)
-
-            if i+1 != self.number_of_states:
-                self.set_equality_constraint("continuity_x"+state_idx, self.states[i][-1].x - self.states[i+1][0].x, 0)
-                self.set_equality_constraint("continuity_y"+state_idx, self.states[i][-1].y - self.states[i+1][0].y, 0)
-                self.set_equality_constraint("continuity_th"+state_idx, self.states[i][-1].theta - self.states[i+1][0].theta, 0)
-
-        self.set_constraint("time_sum", time_squared_sum, 0, self.t_max)
-
-        state_id = 0
-        for state in self.states:
-            stage_id = 0
-            for j in range(1, self.number_of_stages):
-                traj_id = 0
-
-                trajectory = self.trajectory(
-                    state=state[j-1],
-                    v=self.v,
-                    k=self.k,
-                    n=20,
-                    s=state[j].sigma,
-                    t=state[j].t
+            # Ensure to add each state into constraint dict to extract values during inference
+            traj_id = 0
+            for intermediate_state in Xim1_fine:
+                traj_idx = state_idx + str(traj_id)
+                self.set_constraint(
+                    "intermediate_x"+traj_idx,
+                    intermediate_state.x
                 )
-
-                for intermediate_state in trajectory:
-                    idx = str(state_id)+str(stage_id)+str(traj_id)
+                self.set_constraint(
+                    "intermediate_y"+traj_idx,
+                    intermediate_state.y
+                )
+                
+                # Check for any obstacle avoidance here
+                obs_id = 0
+                for obstacle in self.obstacles:
+                    # Distance from center of the circle
+                    check_obstacle = lambda x, y : power(obstacle[0] - x, 2) + power(obstacle[1] - y, 2) - power(obstacle[2]+obstacle[3], 2)
+                    idx = traj_idx + str(obs_id)
                     self.set_constraint(
-                        "intermediate_x"+idx,
-                        intermediate_state.x
+                        "check_obs"+idx,
+                        check_obstacle(intermediate_state.x, intermediate_state.y),
+                        0
                     )
-                    self.set_constraint(
-                        "intermediate_y"+idx,
-                        intermediate_state.y
-                    )
-                    
-                    # Check for obstacles
-                    obs_id = 0
-                    for obstacle in self.obstacles:
-                        # Distance from center of the circle
-                        check_obstacle = lambda x, y : power(obstacle[0] - x, 2) + power(obstacle[1] - y, 2) - power(obstacle[2], 2)
-                        idx = idx + str(obs_id)
-                        self.set_constraint(
-                            "check_obs"+idx,
-                            check_obstacle(intermediate_state.x, intermediate_state.y),
-                            0
-                        )
-                        obs_id += 1
-
-                    traj_id += 1
-                stage_id += 1
-            state_id += 1
+                    obs_id += 1
+                traj_id += 1
 
     @staticmethod
     def trajectory(*args, **kwargs) -> list[State]:
@@ -158,25 +275,30 @@ class DubinCar(Problem):
 
             dx = state.x + v*dt*sinc(arc_phase*dt)*cos(state.theta+arc_phase*dt)
             dy = state.y + v*dt*sinc(arc_phase*dt)*sin(state.theta+arc_phase*dt)
-            dth = state.theta + 2*arc_phase
+            dth = state.theta + 2*arc_phase*dt
 
+            # All redundant values are set to 0
             trajectory.append(State(
-                dx, dy, dth, s, dt
+                dx, dy, dth, 0, 0, 0, 0, 0, 0, 0, 0
             ))
 
         return trajectory
 
     def objective(self, *args, **kwargs):
+        # Distance between two states : Euclidean
+        # TODO: Is this really how I should check path length?
         def length(stateA : State, stateB : State):
             return power((stateA.x - stateB.x), 2) + power((stateA.y - stateB.y), 2)
 
         path_length = 0
-        for i in range(1, len(self.states)):
-            path_length += length(self.states[i][0], self.states[i-1][0])
+        for i in range(1, self.number_of_states):
+            path_length += length(self.states[i], self.states[i-1])
 
         return path_length
 
     def initial_guess(self, *args, **kwargs):
+        # For initial guess, set all variables to random values.
+        # TODO: Improve the initial guess system for faster convergence
         guess_variables = []
         for i in self.get_variables():
             guess_variables.append(DM_rand())
@@ -217,11 +339,11 @@ class DubinCar(Problem):
 
 if __name__ == "__main__":
     init = Coords2D(0.0, 0.0, 0.0)
-    final = Coords2D(1.0, 5.0, 0.0)
+    final = Coords2D(10.0, 10.0, 0.0)
 
     # X, Y, R -> Circular Obstacles
     obstacles = [
-        # [1.0, 5.0, 2.0]
+        [5.0, 5.0, 2.0, 1.0]
     ]
 
     lm = LimoBot()
@@ -230,10 +352,12 @@ if __name__ == "__main__":
     ex = Executor(dc)
     ex.prep(init=init, final=final, obstacles=obstacles)
     solution, solver = ex.solve()
+    
     decision_variables = solution["x"]
     constraints = solution["g"]
 
     import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
 
     intermediate_values = dc.get_constraint_idx_by_pattern("intermediate")
 
@@ -243,7 +367,20 @@ if __name__ == "__main__":
         x_idx = intermediate_values[i]; y_idx = intermediate_values[i+1]
         note_x.append(float(constraints[x_idx])); note_y.append(float(constraints[y_idx]))
 
-    plt.plot(note_x, note_y)
+    fig, ax = plt.subplots()
+
+    for obstacle in obstacles:
+        ax.add_patch(patches.Circle(
+            (obstacle[0], obstacle[1]), obstacle[2],
+            linewidth=1, edgecolor='r', facecolor='none'
+        ))
+
+        ax.add_patch(patches.Circle(
+            (obstacle[0], obstacle[1]), obstacle[2]+obstacle[3],
+            linewidth=1, edgecolor='r', facecolor='none'
+        ))
+
+    ax.plot(note_x, note_y)
     plt.show()
 
     # Generating Control Sequences from State Matrices
